deb9aa175c25fa6967702ba2a2ff2ece3c619ffc
Burnsedia <bburnsed1995@gmail.com>
2024-03-31 20:56:22 -0400

    simplfide code

--- dart/two-fer/lib/two_fer.dart
+++ dart/two-fer/lib/two_fer.dart
@@ -1,10 +1,6 @@
 String twoFer([String name ="you"]) {
   // Replace the throw call and put your code here
-  String defult = "One for you, one for me.";
   String str = "One for $name, one for me."; 
-  if (name == null) {
-    return defult;
-  }
   return str;
   
 }
