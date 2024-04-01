b2eb80ebe4646ac9d2550d8f3f157bc5537bd554
Burnsedia <bburnsed1995@gmail.com>
2024-03-31 20:54:12 -0400

    sytax error, I hate dart sometimes

--- dart/two-fer/lib/two_fer.dart
+++ dart/two-fer/lib/two_fer.dart
@@ -1,4 +1,10 @@
-String twoFer({String name = "you"}) {
+String twoFer([String name ="you"]) {
   // Replace the throw call and put your code here
-  return "One for $name, one for me.";
+  String defult = "One for you, one for me.";
+  String str = "One for $name, one for me."; 
+  if (name == null) {
+    return defult;
+  }
+  return str;
+  
 }
