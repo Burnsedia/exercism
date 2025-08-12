68862106deadb2faad38a564ac27c806be4e6a4d
Burnsedia <bburnsed1995@gmail.com>
2025-08-12 09:56:37 -0400

    setting up basic structure, wrote it like python like a moron

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,3 +1,17 @@
 class Leap {
   // Put your code here
+  bool Leap = false
+
+
+  bool leapyear(year) {
+    if (year % 100 && year % 400) {
+      return Leap = true
+     } 
+    if (year % 4){
+        return Leap = true
+      }
+    else {
+        return Leap = false
+      }
+  }
 }
