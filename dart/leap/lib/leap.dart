9452e3747a3cc6f049f5664725838b5eef11fa01
Burnsedia <bburnsed1995@gmail.com>
2025-08-12 10:04:51 -0400

    my logic was close, Miss read it

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,17 +1,14 @@
 class Leap {
-  // Put your code here
-  bool Leap = false
-
-
-  bool leapyear(year) {
-    if (year % 100 && year % 400) {
-      return Leap = true
-     } 
-    if (year % 4){
-        return Leap = true
-      }
-    else {
-        return Leap = false
-      }
+  bool leapYear(int year) {
+    if (year % 400 == 0) {
+      return true;
+    } else if (year % 100 == 0) {
+      return false;
+    } else if (year % 4 == 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
+
